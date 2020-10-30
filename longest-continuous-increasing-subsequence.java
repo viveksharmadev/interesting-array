bd7c16aa6826971afe699379ff11857a09fecb7d
Learner <vivek.gurukul1982@gmail.com>
2020-10-30 08:35:47 -0700

    Update longest-continuous-increasing-subsequence.java

--- longest-continuous-increasing-subsequence.java
+++ longest-continuous-increasing-subsequence.java
@@ -13,3 +13,28 @@ class Solution {
         return res;
     }
 }
+
+// May be easier to understand
+class Solution {
+    // tc -> n, sc-> 1
+    public int findLengthOfLCIS(int[] nums) {
+        if(nums==null || nums.length==0) return 0;
+        
+        int maxLength = 1;
+        
+        int n = nums.length;
+        
+        int start = 0;
+        for(int i=1; i<n; i++){
+            if(nums[i-1] < nums[i]){
+                maxLength = Math.max(maxLength, i-start+1);
+            }
+            
+            else{
+                start = i;
+            }
+        }
+        
+        return maxLength;
+    }
+}
